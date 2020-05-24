b56a408d5ffd57a11b3e00b9c0c3fde0c5f57e01
Diego Montaño <diegofmp98@gmail.com>
2020-05-23 19:55:40 -0500

    First commit

---
+++ LecturaArchivo.h
@@ -0,0 +1,19 @@
+#ifndef LECTURAARCHIVO_H
+#define LECTURAARCHIVO_H
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class LecturaArchivo{
+    private:
+
+    public:
+
+        inline LecturaArchivo(){
+        }
+
+
+};
+
+
+#endif //   LECTURAARCHIVO_H
---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+Instrucciones: El programa deberá simular un manejador de memoria virtual utilizando paginación parecidoal visto en clase, y medir su rendimiento. 
+Se utilizarán las estrategias de reemplazo FIFO y LRU, para que comparen el rendimiento de ambas estrategias. 
+Asuman que la computadora tiene un solo procesador; una memoria real de 2048 bytes, y que las páginas son de 16 bytes.
+
+Integrantes: Diego Montaño, Jose Gonzalez, Eugenio Gonzalez, Rafael Martinez
+
+*/
+
+
+#include <iostream>
+#include "LecturaArchivo.h"
+using namespace std;
+
+int main(){
+    /*
+    *
+    * 
+    * Code
+    * 
+    * 
+    */
+    return 0;
+}
