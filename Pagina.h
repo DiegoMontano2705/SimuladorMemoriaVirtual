22aa19cdfbcb3a57be9673d0315e1a15b31a76bc
josegzza <a01038061@itesm.mx>
2020-06-14 20:43:00 -0500

    Proceso.h / Queue.h

---
+++ Pagina.h
@@ -0,0 +1,35 @@
+#ifndef PAGINA_H
+#define PAGINA_H
+
+class Pagina{
+    private:
+    int id, iNumPagina;
+    public:
+
+    Pagina(int id, int iNumPagina){
+        this->id=id;
+        this->iNumPagina=iNumPagina;
+    }
+
+    //Sets and gets
+    int get_id(){
+        return id;
+    }
+
+    int get_numPagina(){
+        return iNumPagina;
+    }
+
+    void set_id(int id){
+        this->id=id;
+    }
+
+    void set_numPagina(int iNumPagina){
+        this->iNumPagina=iNumPagina;
+    }
+
+    
+
+
+};
+#endif
